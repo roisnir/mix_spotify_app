0cccb88f6abd62b7388ff8da433b64b8b9810c15
Roi Snir <roisnr132@gmail.com>
2020-03-22 01:37:36 +0200

    SelectTemplate refactor

--- lib/screens/create_project/select_template.dart
+++ lib/screens/create_project/select_template.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/spotify_io.dart';
 import 'package:spotify_manager/common/project_manager/project_template.dart';
+import 'package:spotify_manager/common/utils.dart';
+import 'package:spotify_manager/screens/create_project/create_saved_songs_project.dart';
 import 'package:spotify_manager/screens/create_project/form_fields.dart';
+import 'package:spotify_manager/main.dart';
 
 final templates = <ProjectTemplate>[
   ProjectTemplate(
       "Liked Songs",
       "Go through every song you have ever liked and sort them to your playlists",
-      Icons.favorite),
+      Icons.favorite,
+      builder: (playlists)=>CreateSavedSongsProject(playlists)),
   ProjectTemplate(
       "Discover",
       "Choose tracks you like, get recommendations based on them and sort them to your playlists",
@@ -21,26 +26,65 @@ final templates = <ProjectTemplate>[
       Icons.build),
 ];
 
-Widget pageTemplate(theme, Iterable<ProjectTemplate> templates, Function(int) onChanged) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text("Choose Template", style: theme.textTheme.headline4),
-      Padding(
-        padding: const EdgeInsets.only(top: 4, bottom: 20),
-        child: Text(
-          "What kind of project would you like to start?",
-          style: theme.textTheme.caption,
-        ),
+class SelectTemplate extends StatefulWidget {
+  final SpotifyApi api;
+  final User currentUser;
+
+  SelectTemplate(this.api, this.currentUser);
+
+  @override
+  _SelectTemplateState createState() => _SelectTemplateState();
+}
+
+class _SelectTemplateState extends State<SelectTemplate> {
+  Future<List<PlaylistSimple>> playlistsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    playlistsFuture = widget.api.playlists.me.all().then(
+            (playlists) => playlists.where(
+                    (playlist) => playlist.owner.id == widget.currentUser.id).toList());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SimpleFutureBuilder(
+      playlistsFuture,
+      (context, playlists)=>Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 40),),
+          Text("Choose Template", style: theme.textTheme.headline4),
+          Padding(
+            padding: const EdgeInsets.only(top: 4, bottom: 20),
+            child: Text(
+              "What kind of project would you like to start?",
+              style: theme.textTheme.caption,
+            ),
+          ),
+          Expanded(
+            child: ProjectTemplateSelection(
+              theme: theme,
+              templates: templates,
+              validator: (int i) => i == null ? "please select template" : null,
+              onChanged: (i){
+                final builder = templates[i].builder;
+                if (builder == null){
+                  showDialog(context: context, builder: (context)=>AlertDialog(
+                    title:Text("Coming Soon!"), content: Text("This project template is not supported yet."),actions: <Widget>[FlatButton(child:Text("OK"), onPressed: (){},)],));
+                  return;
+                }
+                Navigator.of(context).push(MaterialPageRoute(builder: (c)=>
+                SpotifyContainer(client: widget.api, myDetails: widget.currentUser, child:
+                  builder(playlists),))).then((project) => Navigator.of(context).pop(project));
+              },
+            ),
+          ),
+        ],
       ),
-      Expanded(
-        child: ProjectTemplateSelection(
-          theme: theme,
-          templates: templates,
-          validator: (int i) => i == null ? "please select template" : null,
-          onChanged: onChanged,
-        ),
-      ),
-    ],
-  );
+    );
+
+  }
 }
