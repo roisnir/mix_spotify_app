81c9981a366dee29813ad50fb5bc140e24262dda
Roi Snir <roisnr132@gmail.com>
2020-03-22 01:24:59 +0200

    fixed toList

--- lib/common/project_manager/create_project.dart
+++ lib/common/project_manager/create_project.dart
@@ -4,7 +4,7 @@ import 'package:spotify_manager/common/project_manager/model/project.dart';
 
 Future<ProjectConfiguration> createSavedSongsProject(SpotifyApi client, Iterable<String> playlistIds, String projectName) async {
   final tracksIds = (await client.tracks.me.saved.all()).map((t)=>t.track.id).toList();
-  final project = ProjectConfiguration.init(projectName, tracksIds, playlistIds);
+  final project = ProjectConfiguration.init(projectName, tracksIds, playlistIds.toList());
   await ProjectsDB().insertProject(project);
   return project;
 }
