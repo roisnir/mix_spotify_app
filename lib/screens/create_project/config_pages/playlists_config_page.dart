a0d8ce67ee4d0104ce809527df487dcf5113fb25
Roi Snir <roisnr132@gmail.com>
2020-03-23 01:55:30 +0200

    changed to stateful widget

--- lib/screens/create_project/config_pages/playlists_config_page.dart
+++ lib/screens/create_project/config_pages/playlists_config_page.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/spotify_io.dart';
+import 'package:spotify_manager/main.dart';
 import '../../new_playlist_dialog.dart';
 import '../form_fields.dart';
 import 'config_page.dart';
 
 
 class PlaylistsConfigPage extends ConfigPage {
-  void Function(List<bool>) onSaved;
-  void Function(List<bool>) onPlaylistAdded;
+  final void Function(List<bool>) onSaved;
   final List<PlaylistSimple> playlists;
   final List<bool> selectedPlaylists;
-  final SpotifyApi client;
-  final String userId;
 
   PlaylistsConfigPage({
     GlobalKey<FormState> key,
     @required this.onSaved,
     @required this.playlists,
-    @required this.selectedPlaylists,
-    this.onPlaylistAdded,
-    @required this.client,
-    @required this.userId}):super(key);
+    @required this.selectedPlaylists}):super(key);
 
   @override
-  Widget buildPage(BuildContext context) {
+  Widget buildPage(BuildContext context) => PlaylistsConfigWidget(playlists, selectedPlaylists, onSaved);
+}
+
+class PlaylistsConfigWidget extends StatefulWidget {
+  final List<PlaylistSimple> playlists;
+  final List<bool> selectedPlaylists;
+  final void Function(List<bool>) onSaved;
+
+  PlaylistsConfigWidget(this.playlists, this.selectedPlaylists, this.onSaved);
+
+  @override
+  _PlaylistsConfigWidgetState createState() => _PlaylistsConfigWidgetState();
+}
+
+class _PlaylistsConfigWidgetState extends State<PlaylistsConfigWidget> {
+  List<PlaylistSimple> playlists;
+  List<bool> selectedPlaylists;
+
+  @override
+  void initState() {
+    super.initState();
+    playlists = widget.playlists;
+    selectedPlaylists = widget.selectedPlaylists;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final spotifyContainer = SpotifyContainer.of(context);
+    final client = spotifyContainer.client;
+    final userId = spotifyContainer.myDetails.id;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -43,8 +67,10 @@ class PlaylistsConfigPage extends ConfigPage {
               child: IconButton(icon: Icon(Icons.add), onPressed: () async {
                 Playlist playlist = await showDialog(context: context, child: NewPlaylistDialog(client, userId));
                 if (playlist != null) {
-                  playlists.add(playlist);
-                  onPlaylistAdded(selectedPlaylists); // TODO: do it better
+                  setState(() {
+                    playlists.add(playlist);
+                    selectedPlaylists.add(true);
+                  });
                 }
               },),
             )
@@ -57,16 +83,15 @@ class PlaylistsConfigPage extends ConfigPage {
             child: PlaylistsSelection(
               playlists: playlists,
               theme: Theme.of(context),
-              onSaved: onSaved,
+              onSaved: widget.onSaved,
               validator: (v) =>
               v.any((e) => e) ? null : "select at least one playlist",
-              initialValue: List.generate(playlists.length, (i) => false),
+              initialValue: selectedPlaylists,
             ),
           ),
         ),
       ],
     );
+
   }
-
-
 }
