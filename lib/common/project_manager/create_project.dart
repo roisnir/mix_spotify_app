66bd0c3332af118aaaede43505753971ac78bc6c
Roi Snir <roisnr132@gmail.com>
2020-03-27 16:04:52 +0300

    added create Discover project

--- lib/common/project_manager/create_project.dart
+++ lib/common/project_manager/create_project.dart
@@ -4,17 +4,53 @@ import 'package:spotify_manager/common/project_manager/projects_db.dart';
 import 'package:spotify_manager/common/project_manager/model/project.dart';
 import 'package:spotify_manager/common/utils.dart';
 
-Future<ProjectConfiguration> createSavedSongsProject(SpotifyApi client, Iterable<String> playlistIds, String projectName) async {
-  final tracksIds = (await client.tracks.me.saved.all()).map((t)=>t.track.id).toList();
-  final project = ProjectConfiguration.init(projectName, tracksIds, playlistIds.toList());
+Future<ProjectConfiguration> createSavedSongsProject(SpotifyApi client,
+    Iterable<String> playlistIds, String projectName) async {
+  final tracksIds = (await client.tracks.me.saved.all())
+      .map((t) => t.track.id)
+      .toList();
+  final project = ProjectConfiguration.init(
+      projectName, tracksIds, playlistIds.toList());
   await ProjectsDB().insertProject(project);
   return project;
 }
 
-Future<ProjectConfiguration> createMaintainProject(SpotifyApi api, List<PlaylistSimple> allPlaylists, Iterable<PlaylistSimple> selectedPlaylists, String projectName) async {
-  final playlists = await Future.wait(allPlaylists.map((p) => ProjectPlaylist.fromSimplePlaylist(p, api)));
-  final List<String> tracksIds = (await api.tracks.me.saved.all()).where((track) => playlists.all((playlist) => !playlist.contains(track.track))).map<String>((t)=>t.track.id).toList();
-  final project = ProjectConfiguration.init(projectName, tracksIds, selectedPlaylists.map((playlist) => playlist.id).toList());
+Future<ProjectConfiguration> createMaintainProject(SpotifyApi api,
+    List<PlaylistSimple> allPlaylists,
+    Iterable<PlaylistSimple> selectedPlaylists, String projectName) async {
+  final playlists = await Future.wait(
+      allPlaylists.map((p) => ProjectPlaylist.fromSimplePlaylist(p, api)));
+  final List<String> tracksIds = (await api.tracks.me.saved.all())
+      .where((track) =>
+      playlists.all((playlist) => !playlist.contains(track.track)))
+      .map<String>((t) => t.track.id)
+      .toList();
+  final project = ProjectConfiguration.init(projectName, tracksIds,
+      selectedPlaylists.map((playlist) => playlist.id).toList());
   await ProjectsDB().insertProject(project);
   return project;
+}
+
+
+Future<ProjectConfiguration> createDiscoverProject(SpotifyApi api,
+    List<String> seedArtists, List<String> seedTracks,
+    Iterable<PlaylistSimple> selectedPlaylists, String projectName,
+    [int limit = 100]) async {
+  final trackIds = await api.recommendations.get(
+      seedArtists: seedArtists,
+      seedTracks: seedTracks,
+      limit: limit,
+      market: 'IL').then((recommendations) =>
+      recommendations.tracks.map((track) => track.id).toList(growable: false));
+  final project = ProjectConfiguration.init(projectName, trackIds, selectedPlaylists.map((playlist) => playlist.id).toList());
+  await ProjectsDB().insertProject(project);
+  return project;
+}
+
+
+Future<ProjectConfiguration> createExtendProject(SpotifyApi api,
+    List<PlaylistSimple> allPlaylists,
+    Iterable<PlaylistSimple> selectedPlaylists, String projectName) async {
+  // TODO: implement extend
+  throw UnimplementedError();
 }
