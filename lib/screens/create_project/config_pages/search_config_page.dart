ef486a82fc99e1d1005d4ba592fbc278dafaa83b
Roi Snir <roisnr132@gmail.com>
2020-03-30 02:02:22 +0300

    extracted search to widget and added search form field

--- lib/screens/create_project/config_pages/search_config_page.dart
+++ lib/screens/create_project/config_pages/search_config_page.dart
@@ -1,160 +1,122 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:spotify/spotify_io.dart' hide Image;
+import 'package:spotify_manager/common/project_manager/model/spotify_items.dart';
 import 'package:spotify_manager/main.dart';
-import 'package:spotify_manager/widgets/image.dart';
+import 'package:spotify_manager/widgets/search.dart';
 import 'config_page.dart';
 
 const maxSeeds = 5;
 
+nullFunc(v) {}
+
 class SearchConfigPage extends ConfigPage {
-  final Function (List<Artist>) onArtistsChange;
-  final Function (List<Track>) onTracksChange;
-
-  SearchConfigPage({GlobalKey<FormState> key, @required this.onArtistsChange, @required this.onTracksChange}) : super(key);
-
-  @override
-  Widget buildPage(BuildContext context) => Search(onArtistsChange, onTracksChange);
-}
-
-class Search extends StatefulWidget {
-  final Function (List<Artist>) onArtistsChange;
-  final Function (List<Track>) onTracksChange;
-
-  Search(this.onArtistsChange, this.onTracksChange);
+  final Function(SpotifyItems) onSaved;
+  SearchConfigPage({GlobalKey<FormState> key, @required this.onSaved})
+      : super(key);
 
   @override
-  _SearchState createState() => _SearchState();
+  Widget buildPage(BuildContext context) => SearchFormField(
+        onSaved: onSaved,
+        validator: (items) => items.length > 0 && items.length <= maxSeeds
+            ? null
+            : "Select at least one item",
+      );
 }
 
-class _SearchState extends State<Search> {
-  Future<SearchResult> searchResultsFuture;
-  List<Artist> selectedArtists = [];
-  List<Track> selectedTracks = [];
+class SearchFormField extends StatefulWidget {
+  final Function(SpotifyItems) onSaved;
+  final Function(SpotifyItems) onChanged;
+  final String Function(SpotifyItems) validator;
 
-  get seedsNum => selectedArtists.length + selectedTracks.length;
+  SearchFormField(
+      {this.onSaved, Function(SpotifyItems) onChanged, this.validator})
+      : this.onChanged = onChanged ?? nullFunc;
 
+  @override
+  _SearchFormFieldState createState() => _SearchFormFieldState();
+}
+
+class _SearchFormFieldState extends State<SearchFormField> with AutomaticKeepAliveClientMixin<SearchFormField>{
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      floatingActionButton: FloatingActionButton(
-        child: Text(seedsNum.toString()),
-        onPressed: (){
-          showModalBottomSheet(context: context, builder: (c) =>
-          StatefulBuilder(
-            builder: (cc, sheetSetState) => Container(child: Column(
-              children: selectedTracks.map<Widget>((track) =>
-                  buildTrackTile(track, trailing: IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: (){
-                      setState((){
-                        selectedTracks.remove(track);
-                      });
-                      widget.onTracksChange(selectedTracks);
-                      sheetSetState((){selectedTracks = selectedTracks;});
-                    },))).toList() +
-                  selectedArtists.map<Widget>((artist) =>
-                      buildArtistTile(artist, trailing: IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: (){
-                          setState((){
-                          selectedArtists.remove(artist);
-                          });
-                          widget.onArtistsChange(selectedArtists);
-                          sheetSetState((){selectedArtists=selectedArtists;});
-                        },))).toList()
-              ,),),
-          ));
-        },),
-      body: Column(children: <Widget>[
-        TextFormField(
-          style: TextStyle(fontSize: 24),
-          textAlign: TextAlign.center,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: theme.bottomAppBarColor,
-            hintText: "Search tracks or artists",
-            helperText: "Select music you like"
-          ),
-          onChanged: (value)=>setState((){
-            searchResultsFuture = search(context, value);
-          })),
-        Padding(padding: EdgeInsets.only(top: 20),),
-        Expanded(
-          child: FutureBuilder(
-            future: searchResultsFuture,
-            initialData: 'null',
-            builder: (context, snapshot){
-              if (snapshot.hasData)
-                return snapshot.data == 'null' ? Container():
-                buildSearchResults(context, snapshot.data);
-              if (snapshot.hasError)
-                return Column(children: <Widget>[Icon(Icons.error),
-                  Text("an error occured, try again later")],);
-              return Center(child: CircularProgressIndicator());
+    super.build(context);
+    return FormField<SpotifyItems>(
+      initialValue: SpotifyItems(),
+      onSaved: widget.onSaved,
+      validator: (items){
+        final errorStr = widget.validator(items);
+        if (errorStr != null)
+          Scaffold.of(context).showSnackBar(SnackBar(content: Text("Select at least one item"),));
+        return errorStr;
+      },
+      builder: (state) {
+        final selectedItems = state.value;
+        return Scaffold(
+          resizeToAvoidBottomPadding: false,
+          floatingActionButton: FloatingActionButton(
+            child: Text(selectedItems.length.toString()),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (c) => StatefulBuilder(
+                        builder: (cc, sheetSetState) => Container(
+                          child: buildBottomSheet(
+                              selectedItems, sheetSetState, state),
+                        ),
+                      ));
             },
           ),
-        )
-      ],),
+          body: Search(
+            api: SpotifyContainer.of(context).client,
+            onTrackTap: (track) => setItems(context, state, ()=>selectedItems..tracks.add(track)),
+            onArtistTap: (artist) => setItems(context, state, ()=>selectedItems..artists.add(artist)),
+          ),
+        );
+      },
     );
   }
 
-  Future<SearchResult> search(BuildContext context, String searchQuery) async {
-    final api = SpotifyContainer.of(context).client;
-    return await api.search.get(
-        searchQuery, [SearchType.artist, SearchType.track]).first(5);
-  }
-
-  buildSearchResults(BuildContext context, SearchResult searchResult) {
-    return ListView(children: searchResultsItems(context, searchResult),);
-  }
-
-  List<Widget> searchResultsItems(BuildContext context, SearchResult searchResult) {
-    addItem<T>(List<T> list, T item, Function(List<T>) onChange){
-      FocusScope.of(context).unfocus();
-      if (seedsNum >= maxSeeds){
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text("Select up to 5 items"),));
-        return;
-      }
-      setState(() {
-        list.add(item);
-      });
-      onChange(list);
+  setItems(BuildContext context, FormFieldState<SpotifyItems> state, SpotifyItems Function() changeState){
+    FocusScope.of(context).requestFocus(FocusNode());
+    if (state.value.length >= maxSeeds){
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text("Select up to 5 items"),));
+      return;
     }
-    final theme = Theme.of(context);
-    final listItems = <Widget>[];
-    listItems.add(ListTile(title: Text("Tracks", style: theme.textTheme.headline6,),));
-    listItems.add(Divider());
-    listItems.addAll(ListTile.divideTiles(
-      context: context,
-        tiles:
-      searchResult.tracks.items.map((track) => buildTrackTile(track, onTap: ()=>addItem<Track>(selectedTracks, track, widget.onTracksChange)))
-    ));
-    listItems.add(ListTile(title: Text("Artists", style: theme.textTheme.headline6,),));
-    listItems.add(Divider());
-    listItems.addAll(ListTile.divideTiles(
-        context: context,
-        tiles:
-    searchResult.artists.items.map((artist)=>buildArtistTile(artist, onTap: ()=>addItem<Artist>(selectedArtists, artist, widget.onArtistsChange)))
-    ));
-    return listItems;
+    final newState = changeState();
+    state.didChange(newState);
+    widget.onChanged(newState);
   }
 
-  ListTile buildTrackTile(Track track, {Widget trailing, Function onTap}) => ListTile(
-    leading: SpotifyImage(images: track.album.images, width: 50, height: 50),
-    title: Text(track.name, softWrap: false),
-    subtitle: Text("Song | ${track.artists.map((artist) => artist.name).join(', ')}", softWrap: false,),
-    trailing: trailing,
-    onTap: onTap
-  );
-  ListTile buildArtistTile(Artist artist, {Widget trailing, Function onTap}) => ListTile(
-    leading: SpotifyImage(images: artist.images, width: 50, height: 50),
-    title: Text(artist.name, softWrap: false),
-    subtitle: Text("Artist", softWrap: false),
-    trailing: trailing,
-    onTap: onTap,
+  Column buildBottomSheet(SpotifyItems selectedItems, StateSetter sheetSetState,
+      FormFieldState<SpotifyItems> state) {
+    return Column(
+      children: selectedItems.tracks
+              .map<Widget>((track) => TrackTile(track,
+                  trailing: IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      sheetSetState(() {
+                        selectedItems.tracks.remove(track);
+                        state.didChange(selectedItems);
+                      });
+                    },
+                  )))
+              .toList() +
+          selectedItems.artists
+              .map<Widget>((artist) => ArtistTile(artist,
+                  trailing: IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      sheetSetState(() {
+                        selectedItems.artists.remove(artist);
+                        state.didChange(selectedItems);
+                      });
+                    },
+                  )))
+              .toList(),
     );
+  }
 
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/screens/create_project/create_discover_project.dart
+++ lib/screens/create_project/create_discover_project.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/spotify_io.dart';
 import 'package:spotify_manager/common/project_manager/create_project.dart';
+import 'package:spotify_manager/common/project_manager/model/spotify_items.dart';
 import 'package:spotify_manager/main.dart';
 import 'package:spotify_manager/screens/create_project/config_pages/search_config_page.dart';
 import 'create_project.dart';
@@ -18,14 +19,13 @@ class CreateDiscoverProject extends StatefulWidget {
 class _CreateDiscoverProjectState extends State<CreateDiscoverProject> {
   String projectName;
   List<PlaylistSimple> selectedPlaylists;
-  List<Artist> selectedArtists = [];
-  List<Track> selectedTracks = [];
+  SpotifyItems selectedItems;
 
   createProject(SpotifyApi api) {
     return createDiscoverProject(
         api,
-        selectedArtists.map((artist) => artist.id).toList(),
-        selectedTracks.map((track) => track.id).toList(),
+        selectedItems.artists.map((artist) => artist.id).toList(),
+        selectedItems.tracks.map((track) => track.id).toList(),
         widget.playlists,
         projectName);
   }
@@ -38,9 +38,7 @@ class _CreateDiscoverProjectState extends State<CreateDiscoverProject> {
         api: spotifyContainer.client,
         playlists: widget.playlists,
         userDetails: spotifyContainer.myDetails,
-        configPages: [SearchConfigPage(
-            onArtistsChange: (artists)=>selectedArtists = artists,
-            onTracksChange: (tracks) => selectedTracks = tracks),],
+        configPages: [SearchConfigPage(onSaved: (value)=>selectedItems = value),],
         onNameSaved: (name)=>projectName = name,
         onPlaylistsSaved: (playlists) => selectedPlaylists = playlists,
         onSubmit: ()=>createProject(spotifyContainer.client));
